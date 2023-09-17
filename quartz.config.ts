eda32aebad3b3208335d1467deb023095616422e
ARPBlueSteel <leonidas98735698@gmail.com>
2023-09-17 00:03:43 -0700

    Quartz sync: Sep 17, 2023, 12:03 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ARPBlueSteel.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
