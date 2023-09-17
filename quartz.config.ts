7590c625fb64e0de8901eca6360db69417d41aec
ARPBlueSteel <leonidas98735698@gmail.com>
2023-09-17 10:46:54 -0700

    Quartz sync: Sep 17, 2023, 10:46 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,8 +14,8 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     theme: {
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Noto Serif",
+        body: "Rubik",
         code: "IBM Plex Mono",
       },
       colors: {
