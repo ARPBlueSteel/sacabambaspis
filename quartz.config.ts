6823b7327679fbe3b3d457a24fb218d7a4b026d0
ARPBlueSteel <leonidas98735698@gmail.com>
2023-09-24 19:16:19 -0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -53,7 +53,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Latex({ renderEngine: "mathjax" }),
       Plugin.Description(),
     ],
     filters: [Plugin.RemoveDrafts()],
