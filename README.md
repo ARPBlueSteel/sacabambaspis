04ccdac3eebd41a4a64ebe7114c3092027a78726
ARPBlueSteel <leonidas98735698@gmail.com>
2023-10-23 17:39:27 -0700

    color and theme changes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# IB Sacabambaspis
+
+This site is intended to be a successor to the rhsibresources site. This is intended to be a continuous project where students build upon the success (and failure) of other students to help others achieve more. This site is proudly made using Quartz V4.
+
 # Quartz v4
 
 > “[One] who works with the door open gets all kinds of interruptions, but [they] also occasionally gets clues as to what the world is and what might be important.” — Richard Hamming
--- quartz.config.ts
+++ quartz.config.ts
@@ -20,12 +20,12 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
+          light: "#eff1f5",
+          lightgray: "#ccd0da",
+          gray: "#bcc0cc",
+          darkgray: "#4c4f69",
+          dark: "#5c5f77",
+          secondary: "#1e66f5",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
