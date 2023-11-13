1a7750ca49c762a1c7e8b14186527aabfe14a12d
ARPBlueSteel <leonidas98735698@gmail.com>
2023-11-12 17:58:33 -0800

    Update latex.ts

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -27,6 +27,7 @@ export const Latex: QuartzTransformerPlugin<Options> = (opts?: Options) => {
           css: [
             // base css
             "https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css",
+            "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/mhchem.min.js"
           ],
           js: [
             {
@@ -34,6 +35,9 @@ export const Latex: QuartzTransformerPlugin<Options> = (opts?: Options) => {
               src: "https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/copy-tex.min.js",
               loadTime: "afterDOMReady",
               contentType: "external",
+              src: "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/mhchem.min.js"
+              loadTime: "afterDOMReady"
+              contentType: "external"
             },
           ],
         }
