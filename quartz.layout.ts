548adc75164704192b2c6332361e2ec3278cfec3
ARPBlueSteel <leonidas98735698@gmail.com>
2023-11-13 16:17:01 -0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      GitHub: "https://github.com/ARPBlueSteel/sacabambaspis",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
@@ -29,7 +29,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
